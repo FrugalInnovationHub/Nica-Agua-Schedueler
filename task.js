d13523501cd793d83f2c2d98afc2a26c14c90643
Giulliano Siviero <gsilvazanottisiviero@scu.edu>
2022-06-17 12:04:13 -0700

    Catch

--- task.js
+++ task.js
@@ -1,9 +1,14 @@
 const downloadFtp = require("./downloadFTP");
 const downloadZip = require("./downloadZip");
 const runRScript = require("./runR");
+const filelog = require("./fileLog");
 
 downloadFtp().then(() => {
   downloadZip().then(() => {
-    runRScript().then()
+    runRScript()
+      .then()
+      .catch(() => {
+        filelog("Error", "Error running R Script");
+      });
   });
 });
