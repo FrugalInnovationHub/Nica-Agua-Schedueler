1dfebe27ad6f7aea92affb5d1f0aa3598eedab4a
Giulliano Siviero <gsilvazanottisiviero@scu.edu>
2022-06-21 19:52:30 -0700

    Change to 5min

--- scheduele.js
+++ scheduele.js
@@ -1,7 +1,7 @@
 var fileLog = require('./fileLog')
 var CronJob = require('cron').CronJob;
 var job = new CronJob(
-	'0 */1 * * * *',
+	'0 */5 * * * *',
 	function() {
         fileLog("Schedueler","Starting Task")
 		require('child_process').fork('task.js')
