bdb0d8689fda339a4712225750532b4ddea52a0a
Giulliano Siviero <gsilvazanottisiviero@scu.edu>
2022-06-21 19:47:14 -0700

    Remake Schedueler

---
+++ scheduele.js
@@ -0,0 +1,12 @@
+var fileLog = require('./fileLog')
+var CronJob = require('cron').CronJob;
+var job = new CronJob(
+	'0 */1 * * * *',
+	function() {
+        fileLog("Schedueler","Starting Task")
+		require('child_process').fork('task.js')
+	},
+	null,
+	true,
+	'America/Los_Angeles'
+);
